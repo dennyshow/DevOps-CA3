678c4fec5851f58a911e6df4bc77d6aac23f551e
Dennyshow <dehindeshogbanmu@yahoo.ie>
2025-01-02 02:09:12 +0000

    fix class issue with testing

--- src/test/java/com/example/calculator/CalculatorServiceTest.java
+++ src/test/java/com/example/calculator/CalculatorServiceTest.java
@@ -1,11 +1,21 @@
 package com.example.calculator;
 
+import com.example.calculator.service.CalculatorService;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 
 public class CalculatorServiceTest {
 
-    private final CalculatorService calculatorService = new CalculatorService();
+    @InjectMocks
+    private CalculatorService calculatorService;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     void testAddition() {
@@ -25,6 +35,12 @@ public class CalculatorServiceTest {
     @Test
     void testDivision() {
         assertEquals(2, calculatorService.calculate(6, 3, "/"));
+    }
+
+    @Test
+    void testDivisionByZero() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> calculatorService.calculate(1, 0, "/"));
+        assertEquals("Division by zero is not allowed.", exception.getMessage());
     }
 
     @Test
