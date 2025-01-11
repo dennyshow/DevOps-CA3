2885af7748553a27993d77463fb13338f9290e08
Dennyshow <dehindeshogbanmu@yahoo.ie>
2025-01-11 16:28:08 +0000

    add chrome driver for selenium test

--- src/test/java/com/example/calculator/selenium/pages/CalculatorPage.java
+++ src/test/java/com/example/calculator/selenium/pages/CalculatorPage.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
+
+
 public class CalculatorPage {
     private WebDriver driver;
     private WebDriverWait wait;
